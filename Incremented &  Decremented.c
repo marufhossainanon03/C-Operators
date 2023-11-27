23b17effc144926723b2ea5af4df4f558de0aec8
MD Maruf Hossain Anon <142920152+marufhossainanon03@users.noreply.github.com>
2023-11-27 18:49:23 +0600

    Create Incremented &  Decremented .c

---
+++ Incremented &  Decremented.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <conio.h>
+int main() {
+    int X ;
+
+
+    printf("Enter the value of : ");
+    scanf("%d", &X);
+
+
+    printf("Incremented ++X: %d\n", ++X);
+
+
+    printf("Decremented --X: %d\n", --X);
+
+
+    printf("Incremented X++: %d\n", X++);
+
+    printf("Decremented X--: %d\n", X--);
+
+    getch ();
+}
+
+
